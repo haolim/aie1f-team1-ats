e4a5a66e0f04818ddf255d826d801cb3f4699069
Hao <xhaolimx@gmail.com>
2026-09-23 23:25:23 +0800

    feat: Add Column component to the Board - derived state to filter based on column's status during render - if column has no cards show "No candidates yet!" message - for each Candidate, render a CandidateCard. React identifies the same card by the candidate id. - each button is set depending on the key (status) - when a card is clicked, navigate to the candidate details for that candidate id

--- src/components/Column.jsx
+++ src/components/Column.jsx
@@ -1,3 +1,4 @@
+import { useNavigate } from "react-router";
 import CandidateCard from "./CandidateCard";
 
 const ACTIONS_BY_STATUS = {
@@ -5,24 +6,28 @@ const ACTIONS_BY_STATUS = {
   scheduled: ["shortlist", "reject"],
   shortlisted: ["offer", "reject"],
   offered: ["reject"],
-  rejected: ["delete"],
 };
 
 export default function Column({ title, candidates, status, onStatusChange }) {
-  console.log(`Candidates list: ${candidates.status}`);
-  const columnCandidate = candidates.filter((c) => c.status === status);
+  const navigate = useNavigate();
+  const columnCandidates = candidates.filter((c) => c.status === status);
   return (
     <section className={`column column-${status}`}>
       <h3 className="column-title">{title}</h3>
-      {columnCandidate.length === 0 ? (
+      {columnCandidates.length === 0 ? (
         <p className="message message-empty">No candidates yet!</p>
       ) : (
-        columnCandidate.map((c) => (
+        columnCandidates.map((c) => (
           <CandidateCard
+            // prevent cards from being mixed up when
+            // it moves to another column
+            // key={} is for React to match each card to the same
+            // card on the next render
+            key={c.id}
             candidate={c}
-            status={c.status}
             actions={ACTIONS_BY_STATUS[status]}
             onStatusChange={onStatusChange}
+            onClick={() => navigate(`/candidates/${c.id}`)}
           />
         ))
       )}
