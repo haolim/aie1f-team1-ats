62aeb365e63f54df0618a0b03c0db8315c0c14e0
Hao <xhaolimx@gmail.com>
2026-09-25 16:33:53 +0800

    feat: Routes for the completed pages
    - add Routes for the completed pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,21 @@
+import { Routes, Route } from "react-router";
+import RootLayout from "./layouts/RootLayout";
+import BoardPage from "./pages/BoardPage";
+import AddCandidatePage from "./pages/AddCandidatePage";
+import CandidateDetailPage from "./pages/CandidateDetailPage";
+import RejectedPage from "./pages/RejectedPage";
+
 function App() {
-  return <h1>Applicant Tracker</h1>;
+  return (
+    <Routes>
+      <Route element={<RootLayout />}>
+        <Route path="/" element={<BoardPage />} />
+        <Route path="/candidates/new" element={<AddCandidatePage />} />
+        <Route path="/candidates/:id" element={<CandidateDetailPage />} />
+        <Route path="/rejected" element={<RejectedPage />} />
+      </Route>
+    </Routes>
+  );
 }
 
 export default App;
