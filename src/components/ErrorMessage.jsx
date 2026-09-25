03512e40217bdd96647180668d53b2c5c2c97fc8
Siew Hoon Ng <nuyuix@gmail.com>
2026-09-25 11:08:53 +0800

    Delete src/components directory

--- src/components/ErrorMessage.jsx
+++
@@ -1,9 +0,0 @@
-export default function ErrorMessage({ message }) {
-  if (!message) return null;
-
-  return (
-    <p role="alert" className="message message-error">
-      {message}
-    </p>
-  );
-}
